541a1107075b8a6854b7c05a7bf4fd56b91f3d77
Kothajagadish22 <19pa1a0345@vishnu.edu.in>
2022-02-05 19:53:24 +0530

    first commit

---
+++ quiz gui game using python.py
@@ -0,0 +1,23 @@
+from random import randint
+from tkinter import *
+from tkinter import  simpledialog,messagebox
+main=Tk()
+main.withdraw()
+questions=['what is capital of germany?',
+           'Who is the president of the US?',
+           'What is the capital of France?',
+           'What is the capital of Spain?']
+answer=["Berlin",
+        "Biden",
+        "Paris",
+        "Madrid"]
+score=0
+for i in range(0,len(questions)):
+    print(questions[i])
+    user_answer=simpledialog.askstring(questions[i],"Answer: ")
+    if user_answer==answer[i]:
+        messagebox.showinfo("Correct","Correct")
+        score=score+1
+    else:
+        messagebox.showinfo("Wrong","Wrong")
+messagebox.showinfo("Score :",str(score))
